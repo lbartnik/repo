20eb5b79effcbd2eca5931f0da0b4e66de2a16bc
Lukasz A. Bartnik <l.bartnik@gmail.com>
2018-07-10 14:18:28 -0700

    fix plotting from png

--- R/plotting.R
+++ R/plotting.R
@@ -3,7 +3,10 @@ print.rawplot <- function (x, ...) {
   ccat(grey = "<rawplot>\n", silver = "# use the plot() method to re-plot\n")
 }
 
+#' @importFrom png readPNG
+#' @importFrom jsonlite base64_dec
 #' @export
 plot.rawplot <- function (x, ...) {
-  grid::grid.raster(x$png)
+  raster <- readPNG(as.raw(base64_dec(x$png)), native = TRUE)
+  grid::grid.raster(raster)
 }
