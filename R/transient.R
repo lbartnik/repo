d6c8aa6d312b8fb74dd2ee08fa2893568279b17a
Lukasz A. Bartnik <l.bartnik@gmail.com>
2018-08-17 08:17:31 -0700

    export tag names and search helpers

--- R/transient.R
+++ R/transient.R
@@ -1,15 +1,74 @@
-#' @rdname history
-ancestor_of <- function(x){}
-
-#' @rdname history
-no_parent <- function(){}
-
-
-
-#' Selection helpers.
+#' Selectors for history queries.
 #'
-#' @rdname selection
+#' @importFrom rlang abort
+#' @rdname history
+#' @export
+ancestor_of <- function(x) {
+  abort("This function should not be called directly")
+}
+
+#' @importFrom rlang abort
+#' @rdname history
+#' @export
+no_parent <- function() {
+  abort("This function should not be called directly")
+}
+
+#' @importFrom rlang abort
+#' @rdname history
+#' @export
+branch_tip <- function() {
+  abort("This function should not be called directly")
+}
+
+#' @description `data_matches` searches for a commit whose artifacts
+#' match objects passed via `...` and `data`.
+#'
+#' @param ... named objects to look for.
+#' @param data (Optional). `...` as a single `list`.
+#'
+#' @importFrom rlang abort
+#' @rdname history
+#' @export
+data_matches <- function(..., data) {
+  abort("This function should not be called directly")
+}
+
+
+
+#' Tag names.
+#'
+#' Tag names need to be declared as objects to silence `R CMD check`.
+#'
+#' @rdname tagnames
+#' @export
+time <- NULL
+
+#' @rdname tagnames
+#' @export
+object <- NULL
+
+#' @rdname tagnames
+#' @export
+plot <- NULL
+
+#' @rdname tagnames
+#' @export
 id <- NULL
 
-#' @rdname selection
+#' @rdname tagnames
+#' @export
 artifact <- NULL
+
+#' @rdname tagnames
+#' @export
+parent_commit <- NULL
+
+#' @rdname tagnames
+#' @export
+parents <- NULL
+
+#' @rdname tagnames
+#' @export
+session <- NULL
+
